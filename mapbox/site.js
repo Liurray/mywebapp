d28342badc59b94aa6b8fc6b33547dd7fcc1a5f6
rray <73302975+alpacalatte@users.noreply.github.com>
2021-04-26 01:21:05 +0800

    改

--- mapbox/site.js
+++ mapbox/site.js
@@ -7,11 +7,41 @@ var map = new mapboxgl.Map({
   attributionControl: {
     position: 'top-left'
   },
-  style: 'mapbox://styles/saman/ciql4uao1000xbkm7knq5qf52',
-  center: [-93.253567, 44.983679],
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [120.673195,24.121166],
   zoom: 17,
   bearing: -9.47,
   pitch: 45.00
+});
+map.on('load', function () {
+  map.addSource('floorplan', {
+      // GeoJSON Data source used in vector tiles, documented at
+      // https://gist.github.com/ryanbaumann/a7d970386ce59d11c16278b90dde094d
+      'type': 'geojson',
+      'data':
+          'https://raw.githubusercontent.com/Liurray/mywebapp/main/mapbox/features.geojson'
+  });
+  map.addLayer({
+      'id': 'room-extrusion',
+      'type': 'fill-extrusion',
+      'source': 'floorplan',
+      'paint': {
+          // See the Mapbox Style Specification for details on data expressions.
+          // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions
+
+          // Get the fill-extrusion-color from the source 'color' property.
+          'fill-extrusion-color': ['get', 'color'],
+
+          // Get fill-extrusion-height from the source 'height' property.
+          'fill-extrusion-height': ['get', 'height'],
+
+          // Get fill-extrusion-base from the source 'base_height' property.
+          'fill-extrusion-base': ['get', 'base_height'],
+
+          // Make extrusions slightly opaque for see through indoor walls.
+          'fill-extrusion-opacity': 0.5
+      }
+  });
 });
 
 var marker;
